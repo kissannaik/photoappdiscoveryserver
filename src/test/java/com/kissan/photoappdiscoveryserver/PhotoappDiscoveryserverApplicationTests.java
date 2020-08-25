22c97f4d7cff9627788291d7e84ea895e9de8235
Kissan Naik <kissannaik@yahoo.com>
2020-08-25 13:24:36 +0530

    Initial commit

---
+++ src/test/java/com/kissan/photoappdiscoveryserver/PhotoappDiscoveryserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kissan.photoappdiscoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoappDiscoveryserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
